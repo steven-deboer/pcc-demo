7c8e48a83d9f90139439a9045b275dc9bd603a1d
Steven <96180461+steven-deboer@users.noreply.github.com>
2023-05-22 16:55:29 +0200

    Update security_checks.sh

--- security_checks.sh
+++ security_checks.sh
@@ -1,63 +1,65 @@
 #!/bin/sh
 
-# Initial message
-echo -e "\033[1;34m
-Welcome to Prisma Cloud Compute Demo
-This script will simulate attacks on your system to test security measures in place
-Prepare to launch sequence...
-\033[0m"
-
-# Function to handle terminal responses
+# Function for response based on command execution status
 terminal_response() {
-    local cmd_exit_status=$1
-    local attack_desc=$2
-    local cmd_output=$3
-
-    if [ $cmd_exit_status -ne 0 ]; then
-        echo -e "\033[1;92m🛡️ Prisma Cloud blocked this attack: $attack_desc. 🛡️ Attack output: $cmd_output\033[0m" # Bright green for blocked attack
+    echo -ne "\n"
+    if [ "$1" -eq 0 ]; then
+        echo -e "\033[38;5;202m⚠️  I was able to execute this attack: $2. ⚠️\033[0m"
+        if [ "$4" -eq 1 ]; then
+            echo -e "\033[38;5;202mAttack output: \n$3\033[0m"
+        fi
     else
-        echo -e "\033[0;33m⚠️ I was able to execute this attack: $attack_desc. ⚠️ Attack output: $cmd_output\033[0m" # Orange for attack
+        echo -e "\033[38;5;34m✅  Prisma Cloud blocked this attack: $2. ✅\033[0m"
     fi
+    sleep 2
 }
 
-# Function for checking /etc/passwd
+# Function to check /etc/passwd
 check_passwd() {
-    echo -e "\033[1;34m⚔️ Launching attack: Reading /etc/passwd\033[0m" # Bright blue for launching attack
+    echo -e "\n\033[1;34m⚔️  Launching attack: Reading /etc/passwd\033[0m"
+    sleep 1
     cmd_output=$(cat /etc/passwd 2>&1)
     cmd_exit_status=$?
-    terminal_response $cmd_exit_status "reading /etc/passwd" "$cmd_output"
+    terminal_response $cmd_exit_status "reading /etc/passwd" "$cmd_output" "$1"
 }
 
-# Function for checking whoami
+# Function to check whoami
 check_whoami() {
-    echo -e "\033[1;34m⚔️ Launching attack: Executing whoami\033[0m" # Bright blue for launching attack
+    echo -e "\n\033[1;34m⚔️  Launching attack: Executing whoami\033[0m"
+    sleep 1
     cmd_output=$(whoami 2>&1)
     cmd_exit_status=$?
-    terminal_response $cmd_exit_status "executing whoami" "$cmd_output"
+    terminal_response $cmd_exit_status "executing whoami" "$cmd_output" "$1"
 }
 
-# Function for Attack Kubernetes API
-attack_kubernetes_api() {
-    echo -e "\033[1;34m⚔️ Launching attack: Attacking Kubernetes API\033[0m" # Bright blue for launching attack
-    cmd_output=$(curl https://kubernetes.default.svc.cluster.local 2>&1)
-    cmd_exit_status=$?
-    terminal_response $cmd_exit_status "Attacking Kubernetes API" "$cmd_output"
-}
-
-# Add more attack functions...
+# Check if verbose mode is enabled
+if [ "$1" == "-v" ]; then
+    verbose_mode=1
+else
+    verbose_mode=0
+fi
 
 while true; do
-    clear
-    echo -e "\033[1;34m################ 🚀 Starting Security Checks 🚀 ################\033[0m" # Blue color for header
+    printf "\033c"
 
-    # List of checks
-    check_passwd
-    check_whoami
-    attack_kubernetes_api
-    # Add other checks...
+    # Top box line
+    echo -e "\033[1;34m
+    ╔════════════════════════════════════════════════════════════════════╗
+    ║      🚀   Starting Security Checks   🚀                            ║
+    ╚════════════════════════════════════════════════════════════════════╝
+    \033[0m"
 
-    echo -e "\033[1;34m################ 🎯 Security Checks Completed 🎯 ################\033[0m" # Blue color for footer
-    echo -e "\033[1;34m💤 Sleeping for 5 seconds before the next round of checks... 💤\033[0m" # Blue color for waiting message
+    check_passwd $verbose_mode
+    check_whoami $verbose_mode
 
-    sleep 5
+    # Progress bar
+    for i in $(seq 20 -1 1)
+    do
+        printf "\r["
+        for j in $(seq $i); do printf "#"; done
+        for j in $(seq $((80-i))); do printf " "; done
+        printf "] \033[1;32m$i\033[0m"
+        sleep 1
+    done
+    printf "\033[2K"
 done
